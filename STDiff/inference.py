f5dd9bc4c90c0045cd6cb789a58626bdb462e342
Jaideep Singh Chawla <jdchawla29@gmail.com>
2024-04-23 01:02:46 +0000

    inference changes

--- STDiff/inference.py
+++ STDiff/inference.py
@@ -2,6 +2,7 @@
 from utils import get_lightning_module_dataloader
 
 import torch
+from torchvision import transforms
 
 from hydra import compose, initialize
 from omegaconf import DictConfig
@@ -54,7 +55,7 @@ def main(cfg : DictConfig) -> None:
     
     def get_resume_batch_idx(r_save_path):
         save_path = Path(r_save_path)
-        saved_preds = sorted(list(save_path.glob('*_0_0.png')))
+        saved_preds = sorted(list(save_path.glob('Pred*')))
         saved_batches = [int(str(p.name).split('_')[1]) for p in saved_preds]
         try:
             return saved_batches[-1]
@@ -65,35 +66,69 @@ def main(cfg : DictConfig) -> None:
     print('number of test batches: ', len(test_loader))
     print('resume batch index: ', resume_batch_idx)
 
+    def visualize_batch_clips(past_batch, future_batch, pred_batch, file_dir):
+        if not Path(file_dir).exists():
+            Path(file_dir).mkdir(parents=True, exist_ok=True) 
+        def save_clip(clip, file_name):
+            imgs = []
+            for i in range(clip.shape[0]):
+                img = transforms.ToPILImage()(clip[i, ...])
+                imgs.append(img)
+
+            imgs[0].save(str(Path(file_name).absolute()), save_all = True, append_images = imgs[1:], loop = 0)
+
+        original_clip = torch.cat([past_batch, future_batch], dim = 1) # shape (N, 2*clip_length, C, H, W)
+        pred_clip = torch.cat([past_batch, pred_batch], dim = 1) # shape (N, 2*clip_length, C, H, W)
+        batch = torch.cat([original_clip, pred_clip], dim = -1) # shape (N, 2*clip_length, C, H, 2W)
+        batch = batch.cpu()
+        N = batch.shape[0]
+        for n in range(N):
+            clip = batch[n, ...]
+            file_name = file_dir.joinpath(f'clip_{n}.gif')
+            save_clip(clip, file_name)
+
     with torch.no_grad():
         progress_bar = tqdm(total=len(test_loader))
         progress_bar.set_description(f"Testing...") 
 
         for idx, batch in enumerate(test_loader):
             if idx > resume_batch_idx: # Resume test
-                Vo, _, Vo_last_frame, _, _ = batch
+                Vo, Vp, Vo_last_frame, _, _ = batch
 
-                images = stdiff_pipeline(
+                pred = stdiff_pipeline(
                     Vo,
                     Vo_last_frame,
                     idx_o,
                     idx_p,
                     num_inference_steps=100,
-                    output_type="numpy"
-                ).images
+                    to_cpu=False
+                )  # Torch Tensor (N, Tp, C, H, W), range (0, 1)
 
-                images_processed = (images * 255).round().astype("uint8")
+                Vo = (Vo / 2 + 0.5).clamp(0, 1)
+                Vp = (Vp / 2 + 0.5).clamp(0, 1)
 
-                for i in range(images_processed.shape[0]):
-                    img = Image.fromarray(images_processed[i])
-                    if (i+1) % Tp == 0:
-                        img.save(Path(r_save_path).joinpath(f'Pred_{idx}_{i//Tp}.png'))
-                progress_bar.update(1)
+                g_pred = accelerator.gather(pred)
+                g_Vo = accelerator.gather(Vo)
+                g_Vp = accelerator.gather(Vp)
+
+                if accelerator.is_main_process:
+                    progress_bar.update(1)
+                    # only for testing, when you have the future frames
+                    visualize_batch_clips(g_Vo, g_Vp, g_pred, file_dir=Path(r_save_path).joinpath(f'Pred_{idx}'))
+
+                    # we want the last frame predicted by the model for the segementation task
+                    for i in range(g_pred.shape[0]):
+                        img = transforms.ToPILImage()(g_pred[i,-1,...])
+                        img.save(Path(r_save_path).joinpath(f'Pred_{idx}/img_{i}.png'))
+
+                    del g_Vo
+                    del g_Vp
+                    del g_pred
         
         progress_bar.close()
-                
+                     
     print("Inference finished")
-    
+
 if __name__ == '__main__':
     config_path = Path(parse_args())
     initialize(version_base=None, config_path=str(config_path.parent))
